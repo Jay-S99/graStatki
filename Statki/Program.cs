8b46fbea281960b2c0a26d59899165cedb3ef4f3
dzw386 <s61280@student.ubb.edu.pl>
2025-01-16 13:58:31 +0100

    Dodaj planszę.

--- Statki/Program.cs
+++ Statki/Program.cs
@@ -2,10 +2,44 @@
 {
     internal class Program
     {
-
+        const int BoardSize = 10;
+        
+        static char[,] CreateBoard()
+        {
+            char[,] board = new char[BoardSize, BoardSize];
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    board[i, j] = '.'; // . oznacza puste pole
+                }
+            }
+            return board;
+        }
+        
+        static void DisplayBoard(char[,] board)
+        {
+            Console.Write(" ");
+            for (int i = 0; i < BoardSize; i++)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+            for(int i = 0; i < BoardSize; i++)
+            {
+                Console.Write(i);
+                for(int j = 0; j < BoardSize; j++)
+                {
+                    Console.Write(" " + board[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char[,] board = CreateBoard();
+            DisplayBoard(board);
         }
     }
 }
