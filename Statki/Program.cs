b01004743181783efca76922526082bed5579228
Jay-S99 <szuranjakub@gmail.com>
2025-01-16 14:52:00 +0100

    ...

--- Statki/Program.cs
+++ Statki/Program.cs
@@ -2,44 +2,152 @@
 {
     internal class Program
     {
-        const int BoardSize = 10;
-        
-        static char[,] CreateBoard()
+        void PlaceShips(char[,] board)
         {
-            char[,] board = new char[BoardSize, BoardSize];
-            for (int i = 0; i < BoardSize; i++)
+            int[] shipSizes = { 5, 4, 3, 3, 2 };
+
+            foreach (int size in shipSizes)
             {
-                for (int j = 0; j < BoardSize; j++)
+                bool placed = false;
+                while (!placed)
                 {
-                    board[i, j] = '.'; // . oznacza puste pole
+                    Console.WriteLine($"Umieść statek o długości {size}: ");
+                    Console.Write("Podaj wiersz: ");
+                    int row = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj kolumne: ");
+                    int col = int.Parse(Console.ReadLine());
+                    Console.Write("Poziomo (h) czy pionowo (v)?: ");
+                    char direction = char.Parse(Console.ReadLine());
+
+                    if (CanPlaceShip(board, row, col, direction))
+                    {
+                        PlaceShips(board, row, col, direction);
+                        placed = true;
+                        DisplayBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie można umieścić statku w tym miejscu! Dawaj jeszcze raz ;)");
+                    }
+                }
+
+
+            }
+
+        }
+        bool CanPlaceShip(char[,] board, int row, int col, int size, char direction)
+        {
+            if (direction == 'h')
+            {
+                if (col + size > BoardSize) { return false; }
+                for (int i = 0; i < size; i++)
+                {
+                    if (board[row, col + i] != '.') { return false; }
                 }
             }
-            return board;
-        }
-        
-        static void DisplayBoard(char[,] board)
-        {
-            Console.Write(" ");
-            for (int i = 0; i < BoardSize; i++)
+            else if (direction == 'v')
             {
-                Console.Write(" " + i);
-            }
-            Console.WriteLine();
-            for(int i = 0; i < BoardSize; i++)
-            {
-                Console.Write(i);
-                for(int j = 0; j < BoardSize; j++)
+                if (row + size > BoardSize) return false;
+                for (int i = 0; i < size; i++)
                 {
-                    Console.Write(" " + board[i, j]);
+                    if (board[row + i, col] != '.') return false;
                 }
-                Console.WriteLine();
+            }
+            return true;
+        }
+
+        void PlaceShip(char[,] board, int row, int col, int size, char direction)
+        {
+            if (direction == 'h')
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    board[row, col + i] = 'S';
+                }
+            }
+            else if (direction == 'v')
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    board[row + i, col] = 'S';
+                }
             }
         }
-        
         static void Main(string[] args)
         {
             char[,] board = CreateBoard();
             DisplayBoard(board);
+        }
+        void PlaceShips(char[,] board)
+        {
+            int[] shipSizes = { 5, 4, 3, 3, 2 };
+
+            foreach (int size in shipSizes)
+            {
+                bool placed = false;
+                while (!placed)
+                {
+                    Console.WriteLine($"Umieść statek o długości {size}: ");
+                    Console.Write("Podaj wiersz: ");
+                    int row = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj kolumne: ");
+                    int col = int.Parse(Console.ReadLine());
+                    Console.Write("Poziomo (h) czy pionowo (v)?: ");
+                    char direction = char.Parse(Console.ReadLine());
+
+                    if (CanPlaceShip(board, row, col, direction))
+                    {
+                        PlaceShips(board, row, col, direction);
+                        placed = true;
+                        DisplayBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie można umieścić statku w tym miejscu! Dawaj jeszcze raz ;)");
+                    }
+                }
+
+
+            }
+
+        }
+        bool CanPlaceShip(char[,] board, int row, int col, int size, char direction)
+        {
+            if (direction == 'h')
+            {
+                if (col + size > BoardSize) { return false; }
+                for (int i = 0; i < size; i++)
+                {
+                    if (board[row, col + i] != '.') { return false; }
+                }
+            }
+            else if (direction == 'v')
+            {
+                if (row + size > BoardSize) return false;
+                for (int i = 0; i < size; i++)
+                {
+                    if (board[row + i, col] != '.') return false;
+                }
+            }
+            return true;
+        }
+
+        void PlaceShip(char[,] board, int row, int col, int size, char direction)
+        {
+            if (direction == 'h')
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    board[row, col + i] = 'S';
+                }
+            }
+            else if (direction == 'v')
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    board[row + i, col] = 'S';
+                }
+            }
         }
     }
 }
